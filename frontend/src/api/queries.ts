de8a9fd2ebd52be8e642114ecec03345dadb2521
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2024-08-28 11:10:35 +1200

    chore: Add queries.ts file for storing queries to the backend

--- frontend/src/api/queries.ts
+++ frontend/src/api/queries.ts
@@ -1,12 +1,14 @@
-export default async function fetchApplicationData(uuid: string) {
+import { ApplicationData } from "../types";
+
+export default async function fetchApplicationData(uuid: string): Promise<ApplicationData> {
     return  {
         uuid: uuid,
         img: "https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png",
-        name: "Google",
-        job: "Graduate Software Engineer",
-        org: "",
+        job: "Software Engineering Director",
+        company: "Google",
+        description: "As the Software Engineering Director for Cloud Observability in Hyderabad, you'll be the foundation of our team in India, responsible for hiring the team and developing them into a high-performing engineering organization. You'll oversee the migration of existing Application Monitoring projects (Cloud Error Reporting, Uptime Monitoring, and Cloud Profiler) to the team in Hyderabad, ensuring continuity for our customers and stakeholders. You'll also ramp up a new effort to improve the usability and integration of third-party observability tools with Google Cloud and Gemini Cloud Assist, our intelligent assistant that helps Google Cloud Platform (GCP) customers operate and troubleshoot their production services.",
         status: "Applied",
-        date: "23/04/18",
-        link: "https://www.google.com",
+        date: "2024/08/27",
+        link: "https://www.google.com/about/careers/applications/jobs/results/99640647849255622-software-engineering-director-cloud-observability?q=Engineering#!t=jo&jid=127025001&",
     }
 }
