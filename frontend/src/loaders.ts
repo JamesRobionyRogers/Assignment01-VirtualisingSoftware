ee9c9692bb9e9b1b545ca141ad6c3ba15f853f97
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2024-09-07 17:31:36 +1200

    refactor: Update loaders.ts to query database for applicationLoader

--- frontend/src/loaders.ts
+++ frontend/src/loaders.ts
@@ -13,28 +13,23 @@ const headers = {
 export async function applicationLoader({ params }: LoaderFunctionArgs) {
     const { uuid } = params;
 
-    // DEBUGGING: Returning test data 
-    return  {
-        uuid: uuid,
-        img: "https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png",
-        job: "Software Engineering Director",
-        company: "Google",
-        description: "As the Software Engineering Director for Cloud Observability in Hyderabad, you'll be the foundation of our team in India, responsible for hiring the team and developing them into a high-performing engineering organization. You'll oversee the migration of existing Application Monitoring projects (Cloud Error Reporting, Uptime Monitoring, and Cloud Profiler) to the team in Hyderabad, ensuring continuity for our customers and stakeholders. You'll also ramp up a new effort to improve the usability and integration of third-party observability tools with Google Cloud and Gemini Cloud Assist, our intelligent assistant that helps Google Cloud Platform (GCP) customers operate and troubleshoot their production services.",
-        status: "Applied",
-        date: "2024/08/27",
-        link: "https://www.google.com/about/careers/applications/jobs/results/99640647849255622-software-engineering-director-cloud-observability?q=Engineering#!t=jo&jid=127025001&",
-        notes: "these are the notes"
+    if (!uuid) return json({});
+
+    try {
+        const response = await axios.get(APIConstants.APPLICATION(uuid), { headers });
+
+        if (!response.data) {
+            return json({ error: `Failed to fetch application with id = ${uuid}` });
+        }
+
+        return json(response.data);
     }
+    catch (error) {
+        const errorMessage = (error as AxiosError).response.data.error || "Failed to fetch application.";
 
-
-
-    // if (!uuid) return json({});
-
-    // TODO: Implement the fetchApplicationData function using axios
-    // const data = await fetchApplicationData(uuid);
-    // if (!data) throw new Response('Not Found', { status: 404 });
-
-    // return json(data);
+        console.error(errorMessage);
+        return json({ error: errorMessage });
+    }
 }
 
 export async function allApplicationLoader() {
